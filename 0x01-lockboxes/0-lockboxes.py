c641cef274d2d49181ae5a85f8eb53a8cdedaa78
reckafella <ethanmuthoniw6@gmail.com>
2023-12-08 00:22:25 +0300

    update 0-lockboxes.py

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -6,13 +6,12 @@ Module contains a method that determines if all the boxes can be opened
 
 def canUnlockAll(boxes):
     ''' determines if all the boxes can be opened '''
-    opened_boxes = [0]
-    for box_id, box in enumerate(boxes):
-        if not box:
-            continue
-        for key in box:
-            if key < len(boxes) and key not in opened_boxes and\
-                    key != box_id:
-                opened_boxes.append(key)
-    print(opened_boxes)
-    return len(opened_boxes) == len(boxes)
+    for key in range(1, len(boxes)):
+        unlocked = False
+        for box in range(len(boxes)):
+            if key in boxes[box] and box != key:
+                unlocked = True
+                break
+        if not unlocked:
+            return False
+    return True

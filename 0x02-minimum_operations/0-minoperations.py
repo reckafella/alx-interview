f1e1378cbcf2d74eb478d5d3659085ee9b165682
reckafella <ethanmuthoniw6@gmail.com>
2023-12-14 23:16:13 +0300

    update 0-minoperations.py

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -11,12 +11,12 @@ def minOperations(n):
     a method that calculates the fewest number of operations needed to result\
             in exactly n H characters in the file.
     '''
-    if (n <= 1):
-        return (0)
+    if n <= 1:
+        return 0
 
-    num_hs = 2  # initial number of H's
-    paste_count = 1  # initial number of pastes
-    operations = 2  # initial number of operations
+    num_hs = 2
+    paste_count = 1
+    operations = 2
  
     while (num_hs < n):
         if (n - num_hs) % num_hs != 0:

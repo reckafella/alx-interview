8e822b21361b3f016e9309c292314f9c4bf3f7e6
reckafella <ethanmuthoniw6@gmail.com>
2023-12-22 01:37:03 +0300

    done with task 0

---
+++ 0x03-log_parsing/0-stats.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+'''
+log parsing module code
+'''
+import re
+import sys
+
+
+def create_log_dict() -> dict:
+    '''
+    create an initial dict of file size and status codes
+    '''
+    status_codes: list = [200, 301, 400, 401, 403, 404, 405, 500]
+
+    log_dict: dict = {
+        'file_size': 0,
+        'status_codes': {str(code): 0 for code in status_codes}
+    }
+
+    return log_dict
+
+
+def parse_line(line: str, regx: re.Pattern[str], log_dict: dict) -> dict:
+    '''
+    parses a single line to increment file_size and status_codes
+    '''
+    match = regx.fullmatch(line)
+
+    if match:
+        status_code, file_size = match.group(1, 2)
+        
+        log_dict['file_size'] += int(file_size)
+        
+        if status_code.isdecimal():
+            log_dict['status_codes'][status_code] += 1
+
+    return log_dict
+
+
+def print_result(log_dict: dict) -> None:
+    '''
+    handles the printing of file_size and status_codes
+    '''
+    print('File size: {}'.format(log_dict['file_size']))
+
+    sorted_codes: list = sorted(log_dict['status_codes'])
+
+    for status_code in sorted_codes:
+        if log_dict['status_codes'][status_code]:
+            print(f"{status_code}: {log_dict['status_codes'][status_code]}")
+
+def execute():
+    #ip = '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+    #date = '\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\]'
+    #get = '"GET /projects/260 HTTP/1.1"'
+    regx = re.compile(r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3} - \[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}.\d+\] "GET /projects/260 HTTP/1.1" (.{3}) (\d+)')
+    # regx = re.compile(r'{} - {} {} (.{3}) (\d+)'.format(ip, date, get))
+
+    log_dict: dict = create_log_dict()
+    
+    line_count: int = 0
+
+    for line in sys.stdin:
+        line = line.strip()
+        
+        line_count = line_count + 1
+
+        parsed_dict: dict = parse_line(line, regx, log_dict)
+
+        try:
+            if line_count % 10 == 0:
+                print_result(parsed_dict)
+        except KeyboardInterrupt:
+            print_result(parsed_dict)
+        
+if __name__ == '__main__':
+    execute()

164a452005377729f0b5086c2cf495cb50ef3898
Reckafella <ethanmuthoni@gmail.com>
2024-01-25 14:52:56 +0300

    fix code not working

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -13,3 +13,7 @@ def rotate_2d_matrix(matrix):
     for i in range(N):
         for j in range(i+1, N):
             matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+
+    for i in range(N):
+        for j in range(int(N/2)):
+            matrix[i][j], matrix[i][N-j-1] = matrix[i][N-j-1], matrix[i][j]
